d067da456eed8ac1f2021002e9f5662a77a67ce0
Giorgio Murad <48260499+giorgiomurad@users.noreply.github.com>
2024-07-31 10:13:26 +0300

    problem3_11.update

--- Projects/Introduction to Java Programming/Chapter3 Selections/Problem3_11.java
+++ Projects/Introduction to Java Programming/Chapter3 Selections/Problem3_11.java
@@ -1,121 +1,139 @@
+package com.company;
+
 // Problem taken from Introduction to Java Programming by Y. Daniel Yang (8th ed.)
 // Chapter 3 : Selections
 //
 // Problem 3.11 : Write a program that asks the user to enter a month number and a year,
 //                and displays how many days this month has.
 //
+//                Note: A year that is divisible by four and one-hundred, or is only divisible by four-hundred
+//                      is a leap year.
+//                      In every leap year, February has 29 days.
+//
 //
 // Author : Giorgio Murad
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in); // Scanner reference to read user input
-        String monthName;                       // Name of the month
-        int monthNB;                            // Month Number
-        int year;                               // Year
-        int days;                               // Number of days
-        boolean valid;                          // True if the user's input is valid, false otherwise
-        boolean leapYear;                       // True if the year is a leap year, false otherwise
+        Scanner input = new Scanner(System.in);     // Scanner reference to read user input
+        String monthName;                           // Name of the month
+        int nbOfDays;                               // Number of days
+        int monthNB;                                // Month number given by the user
+        int year;                                   // Year given by the user
+        boolean isLeapYear;                         // Verifies if the given year is a leap year or not
+        boolean isValid;                            // Verifies if the input values are valid
 
 
-        // Prompting the user to enter month and year
-        System.out.println("Enter the month number (1-12) and year");
+        // Prompting the user to input the month number followed by the year
+        System.out.println("Enter the month number and the year:");
         monthNB = input.nextInt();
-        year    = input.nextInt();
+        year = input.nextInt();
 
-        // Verifying if the year is a leap year or not
-        leapYear = ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0));
+        // Verifying if the inputted year is a leap year of not
+        isLeapYear = ((year % 4 == 0 && year % 100 == 0) || year % 400 == 0);
 
-        // Deducing the number of days in the given month
-        valid = true;
+        // Calculating Results
         switch (monthNB) {
             case 1: {
                 monthName = "January";
-                days      = 31;
+                nbOfDays = 31;
+
+                isValid = true;
                 break;
             }
-
             case 2: {
                 monthName = "February";
-                days      = (leapYear)?29:28;
+                nbOfDays = (isLeapYear) ? 29 : 28;
+
+                isValid = true;
                 break;
             }
-
             case 3: {
                 monthName = "March";
-                days      = 31;
+                nbOfDays = 31;
+
+                isValid = true;
                 break;
             }
-
             case 4: {
                 monthName = "April";
-                days      = 30;
+                nbOfDays = 30;
+
+                isValid = true;
                 break;
             }
-
             case 5: {
                 monthName = "May";
-                days      = 31;
+                nbOfDays = 31;
+
+                isValid = true;
                 break;
             }
-
             case 6: {
                 monthName = "June";
-                days      = 30;
+                nbOfDays = 30;
+
+                isValid = true;
                 break;
             }
-
             case 7: {
                 monthName = "July";
-                days      = 31;
+                nbOfDays = 31;
+
+                isValid = true;
                 break;
             }
-
             case 8: {
                 monthName = "August";
-                days      = 31;
+                nbOfDays = 31;
+
+                isValid = true;
                 break;
             }
-
             case 9: {
                 monthName = "September";
-                days      = 30;
+                nbOfDays = 30;
+
+                isValid = true;
                 break;
             }
-
             case 10: {
                 monthName = "October";
-                days      = 31;
+                nbOfDays = 31;
+
+                isValid = true;
                 break;
             }
-
             case 11: {
                 monthName = "November";
-                days      = 30;
+                nbOfDays = 30;
+
+                isValid = true;
                 break;
             }
-
             case 12: {
                 monthName = "December";
-                days      = 31;
+                nbOfDays = 31;
+
+                isValid = true;
                 break;
             }
-
-            default:{
-                valid     = false;
+            default: {
                 monthName = "";
-                days      = 0;
+                nbOfDays = 0;
+                isValid = false;
             }
         }
 
-
-        // Displaying Result
-        if (valid)
-            System.out.printf("%s %d has %d days", monthName, year, days);
+        // Displaying the result
+        if (isValid)
+            System.out.printf("%s has %d days.", monthName, nbOfDays);
         else
-            System.out.printf("Invalid Month Number. \nA year usually has 12 months.");
+            System.out.println("Invalid month number. (A year usually has 12 months)");
 
+        // Closing the input
         input.close();
     }
 }
